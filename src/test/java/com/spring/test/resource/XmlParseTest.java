5f1fba78411be7dcb31d96bde50aba7cf2e01da7
chengcanmm77 <937236042@qq.com>
2016-06-28 11:15:49 +0800

    添加单元测试

---
+++ src/test/java/com/spring/test/resource/XmlParseTest.java
@@ -0,0 +1,14 @@
+package com.spring.test.resource;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class XmlParseTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
